c65012b75be94f8468432694659ef9ed1c0f0c5b
OopsMonk <oopsmonk@gmail.com>
2014-12-21 23:07:14 +0800

    Add bootstrap-daterangepicker v1.3.17 Test bootstrap-daterangepicker in system page.

--- web-server/assets/sysgraph.js
+++ web-server/assets/sysgraph.js
@@ -4,6 +4,67 @@ darw system graphics
 
 $(document).ready(function(){
 
+    function cpu_date(){
+        
+        var cb = function(start, end, label) {
+            console.log(start.toISOString(), end.toISOString(), label);
+            $('#CPUDateRange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+            //alert("Callback has fired: [" + start.format('MMMM D, YYYY') + " to " + end.format('MMMM D, YYYY') + ", label = " + label + "]");
+        }
+
+        var optionSet1 = {
+        startDate: moment().subtract(29, 'days'),
+        endDate: moment(),
+        minDate: '01/01/2012',
+        maxDate: '12/31/2014',
+        dateLimit: { days: 60 },
+        showDropdowns: true,
+        showWeekNumbers: true,
+        timePicker: false,
+        timePickerIncrement: 1,
+        timePicker12Hour: true,
+        ranges: {
+           'Today': [moment(), moment()],
+           'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+           'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+           'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+           'This Month': [moment().startOf('month'), moment().endOf('month')],
+           'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        },
+        opens: 'left',
+        buttonClasses: ['btn btn-default'],
+        applyClass: 'btn-small btn-primary',
+        cancelClass: 'btn-small',
+        format: 'MM/DD/YYYY',
+        separator: ' to ',
+        locale: {
+            applyLabel: 'Submit',
+            cancelLabel: 'Clear',
+            fromLabel: 'From',
+            toLabel: 'To',
+            customRangeLabel: 'Custom',
+            daysOfWeek: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr','Sa'],
+            monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+            firstDay: 1
+        }
+        };
+
+        $('#CPUDateRange span').html(moment().subtract(29, 'days').format('MMMM D, YYYY') + ' - ' + moment().format('MMMM D, YYYY'));
+
+        $('#CPUDateRange').daterangepicker(optionSet1, cb);
+
+        $('#CPUDateRange').on('show.daterangepicker', function() { console.log("show event fired"); });
+        $('#CPUDateRange').on('hide.daterangepicker', function() { console.log("hide event fired"); });
+        $('#CPUDateRange').on('apply.daterangepicker', function(ev, picker) { 
+        console.log("apply event fired, start/end dates are " 
+          + picker.startDate.format('MMMM D, YYYY') 
+          + " to " 
+          + picker.endDate.format('MMMM D, YYYY')
+        ); 
+        });
+        $('#CPUDateRange').on('cancel.daterangepicker', function(ev, picker) { console.log("cancel event fired"); });
+    }
+
     function draw_cpu(tag_id, rrd_file) {
 
         var gtype_format={'cputemp':{ title: 'Temperature', label:'Temp',color: "#ff8000", checked:true, lines:{
@@ -114,5 +175,6 @@ $(document).ready(function(){
     draw_mem("graphMem", "data/meminfo.rrd");
     draw_uptime("graphUptime", "data/uptime.rrd");
 
+    cpu_date(); 
 }); //EOF ready 
 
